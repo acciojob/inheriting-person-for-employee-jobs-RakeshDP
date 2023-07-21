1c312330c8a077a09e91506aae2a0304c8bc15fa
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-22 00:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	
+}
+Person.prototype.greet = function greet(name,age){
+	console.log(Hello, my name is ${name}, I am ${age} years old.)
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	
+}
+Employee.prototype.jobGreet = function jobGreet(name, age, jobTitle){
+	console.log(Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.);
+}
+Person.prototype.__proto__ = Employee.prototype;
+
 
 // Do not change code below this line
 window.Person = Person;
