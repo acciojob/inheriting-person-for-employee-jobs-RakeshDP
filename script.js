5fe590e282526a1bf6916ad073f2826da891148f
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-22 00:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-// complete this js code
+// // complete this js code
+// function Person(name, age) {
+// 	this.name=name;
+// 	this.age=age;
+// }
+// Person.prototype.greet = function(){
+// 	console.log(Hello, my name is ${this.name}, I am ${this.age} years old.)
+// }
+
+// function Employee(name, age, jobTitle) {
+// 	Person.call(this,name,age);
+// 	this.jobTitle=jobTitle;
+// }
+// Employee.prototype = Object.create(Person.prototype);
+
+// Employee.prototype.constructor = Employee;
+
+// Employee.prototype.jobGreet = function(){
+// 	console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.)
+// }
+
+// Person constructor function
 function Person(name, age) {
-	this.name=name;
-	this.age=age;
-}
-Person.prototype.greet = function(){
-	console.log(Hello, my name is ${this.name}, I am ${this.age} years old.)
+  this.name = name;
+  this.age = age;
 }
 
-function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle=jobTitle;
+// Adding the greet method to the Person prototype
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
+
+// Employee constructor function inheriting from Person
+function Employee(name, age, jobTitle) {
+  // Call the Person constructor using the Employee context
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype into the Employee prototype
 Employee.prototype = Object.create(Person.prototype);
 
+// Set the Employee constructor correctly
 Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet = function(){
-	console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.)
+// Adding the jobGreet method to the Employee prototype
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 
