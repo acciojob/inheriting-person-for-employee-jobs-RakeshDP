7f8659a699bb1347c558b8f67a4f1e8be4172da3
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-29 01:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function Employee(name, age, jobTitle) {
 	this.jobTitle = jobTitle;
 }
 
-// Employee.prototype = Object.create(Person.prototype);
-// Employee.prototype.constructor = Employee;
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
 Employee.prototype._proto_ = Person.prototype;
 
