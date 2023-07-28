703750945edc2274c60d0614687dfdcf06d2a0fe
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-07-28 22:40:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// // complete this js code
-// function Person(name, age) {
-// 	this.name=name;
-// 	this.age=age;
-// }
-// Person.prototype.greet = function(){
-// 	console.log(Hello, my name is ${this.name}, I am ${this.age} years old.)
-// }
-
-// function Employee(name, age, jobTitle) {
-// 	Person.call(this,name,age);
-// 	this.jobTitle=jobTitle;
-// }
-// Employee.prototype = Object.create(Person.prototype);
-
-// Employee.prototype.constructor = Employee;
-
-// Employee.prototype.jobGreet = function(){
-// 	console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.)
-// }
-
 // Person constructor function
 function Person(name, age) {
   this.name = name;
@@ -47,7 +26,12 @@ Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
+const person1 = new Person('John',30);
+person1.greet();
 
+const employee1 = new Employee('Anni', 40, 'Manager');
+employee1.greet();
+employee1.jobGreet();
 
 
 // Do not change code below this line
